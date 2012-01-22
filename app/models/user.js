9be981f3b4db7e9b85d4ef2bdc95893037d825f5
Attila Györffy <attila@attilagyorffy.com>
2012-01-22 14:36:28 +0000

    Refactor User so that it is an actual User object rather than simple JSON.

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,10 @@
-var Users = [
+var User = function User(parameters) {
+  for (var i in parameters) {
+    this[i] = parameters[i];
+  }
+};
+
+var simple_database = [
   { id: 1, name: "Attila" },
   { id: 2, name: "Dain" },
   { id: 3, name: "Rob" }
@@ -6,10 +12,10 @@ var Users = [
 
 module.exports = {
   find: function(id) {
-    for (var i in Users) {
-      var user = Users[i];
-      if (user.id == id) {
-        return user;
+    for (var i in simple_database) {
+      var record = simple_database[i];
+      if (record.id == id) {
+        return new User(record);
       }
     }
     return null;
--- test/models/user-test.js
+++ test/models/user-test.js
@@ -1,14 +1,21 @@
 var vows    = require('vows'),
     assert  = require('assert'),
-    User    = require('../../app/models/user');
+    User    = require('../../app/models/user'),
+    util    = require('util');
 
 vows.describe('User').addBatch({
   '.find': {
     topic: User,
-    'with a valid id': function(topic) {
-      var expected = { id: 1, name: "Attila" };
-      var actual = topic.find(1)
-      assert.equal(actual, expected);
+    'with a valid id': {
+      'should return an instance of User': function(topic) {
+        var record = topic.find(1);
+        assert.ok(record instanceof User); // breaks with: TypeError: Expecting a function in instanceof check, but got #<User>
+      },
+      'should return the correct record': function(topic) {
+        var record = topic.find(1);
+        assert.equal(record.id, 1);
+        assert.equal(record.name, 'Attila');
+      }
     },
     'with an invalid id': function(topic) {
       var actual = topic.find('invalid');
